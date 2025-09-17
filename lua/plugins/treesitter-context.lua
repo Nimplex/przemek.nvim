976d464ade282c1ab2546d04e9ef41c075c0d9f2
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-09-17 16:57:35 +0200

    Add keybind to toggle context

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -5,6 +5,8 @@ return {
 			vim.keymap.set("n", "<leader>c", function()
 				require("treesitter-context").go_to_context(vim.v.count1)
 			end, { silent = true, desc = "Go to Treesitter context" })
+
+            vim.keymap.set("n", "<leader>tc", "<cmd>TSContext toggle<CR>", { desc = "Toggle Treesitter context" })
 		end
 	}
 }
