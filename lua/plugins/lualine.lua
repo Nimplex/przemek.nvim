d16bc3f2d24543e28ede9272954b14312b51e9a0
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-09-18 11:09:16 +0200

    Fix indentation

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,15 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = {
-					icons_enabled = true,
-					component_separators = { left = '|', right = '|'},
-					section_separators = { left = '', right = ''},
-				}
-			})
-		end
-	}
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    icons_enabled = true,
+                    component_separators = { left = '|', right = '|'},
+                    section_separators = { left = '', right = ''},
+                }
+            })
+        end
+    }
 }
--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			vim.keymap.set("n", "<leader>c", function()
-				require("treesitter-context").go_to_context(vim.v.count1)
-			end, { silent = true, desc = "Go to Treesitter context" })
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            vim.keymap.set("n", "<leader>c", function()
+                require("treesitter-context").go_to_context(vim.v.count1)
+            end, { silent = true, desc = "Go to Treesitter context" })
 
             vim.keymap.set("n", "<leader>tc", "<cmd>TSContext toggle<CR>", { desc = "Toggle Treesitter context" })
-		end
-	}
+        end
+    }
 }
